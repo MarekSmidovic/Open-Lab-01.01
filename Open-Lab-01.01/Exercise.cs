56b332ffdb4c6fe8b929f1f93d558f489385aa32
smidovic <msmidovic@gmail.com>
2021-11-07 18:14:28 +0100

    skusam

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -7,7 +7,7 @@ namespace Open_Lab_01._01
         public bool IsLessThan10(string input)
         {
             int a = int.Parse(input);
-            if(a < 10)
+            if (a < 10)
                 return true;
             else
                 return false;
